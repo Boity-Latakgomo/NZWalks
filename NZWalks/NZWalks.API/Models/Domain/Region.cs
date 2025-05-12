498430209ca4c711d69c8c2c7de727f92c87c569
Boitumelo Latakgomo <boitylatakgomo@gmail.com>
2025-05-12 10:17:12 +0200

    Add EF Core setup and domain models for NZWalks API
    Added EF Core package references to `NZWalks.API.csproj` to enable
    database interactions. Introduced `NZWalksDbContext` with `DbSet`
    properties for `Regions`, `Walks`, and `WalkDifficulty`.
    
    Created domain models:
    - `Region` with properties like `Id`, `Code`, `Name`, and a
      navigation property `Walks` for one-to-many relationships.
    - `Walk` with properties like `Id`, `Name`, `Length`, and
      navigation properties for `Region` and `WalkDifficulty`.
    - `WalkDifficulty` with properties `Id` and `Code`.

---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,19 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+
+        //Navigation property :A Region can have many Walks
+        // IEnumerable<Walk> type is used to represent a collection of related Walk entities in a one-to-many relationship.
+        public IEnumerable<Walk> Walks { get; set; }
+
+
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+
+        //RegionId is a foreign key that stores the Id of the related Region. It's just a Guid used by EF Core and the database to link the walk to a region.
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //This is a navigation property.It allows you to access the full Region object related to the Walk in your C# code (e.g., walk.Region.Name).
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
